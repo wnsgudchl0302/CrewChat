ac15af1449bf5d02b599393ae2e20d7061f4ac8b
wnsgudchl0302 <wnsgudchl0302@naver.com>
2022-01-21 15:19:18 +0900

    Initial commit

---
+++ src/main/java/com/jun/crewchat/CrewChatApplication.java
@@ -0,0 +1,13 @@
+package com.jun.crewchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrewChatApplication.class, args);
+    }
+
+}
